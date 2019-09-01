d7a355ce2bc501d8b6481e52d5d425b5f8064b44
cunliff <cunliff@users.noreply.github.com>
2019-09-01 21:43:42 +0200

    lkjh

--- script.js
+++ script.js
@@ -1,37 +1,12 @@
-
-var $tickerWrapper = $(".tickerwrapper");
-var $list = $tickerWrapper.find("ul.list");
-var $clonedList = $list.clone();
-var listWidth = 10;
-
-$list.find("li").each(function (i) {
-			listWidth += $(this, i).outerWidth(true);
-});
-
-var endPos = $tickerWrapper.width() - listWidth;
-
-$list.add($clonedList).css({
-	"width" : listWidth + "px"
-});
-
-$clonedList.addClass("cloned").appendTo($tickerWrapper);
-
-//TimelineMax
-var infinite = new TimelineMax({repeat: -1, paused: true});
-var time = 40;
-
-infinite
-  .fromTo($list, time, {rotation:0.01,x:0}, {force3D:true, x: -listWidth, ease: Linear.easeNone}, 0)
-  .fromTo($clonedList, time, {rotation:0.01, x:listWidth}, {force3D:true, x:0, ease: Linear.easeNone}, 0)
-  .set($list, {force3D:true, rotation:0.01, x: listWidth})
-  .to($clonedList, time, {force3D:true, rotation:0.01, x: -listWidth, ease: Linear.easeNone}, time)
-  .to($list, time, {force3D:true, rotation:0.01, x: 0, ease: Linear.easeNone}, time)
-  .progress(1).progress(0)
-  .play();
-
-//Pause/Play		
-$tickerWrapper.on("mouseenter", function(){
-	infinite.pause();
-}).on("mouseleave", function(){
-	infinite.play();
+$('.marquee').marquee({
+    //speed in milliseconds of the marquee
+    duration: 5000,
+    //gap in pixels between the tickers
+    gap: 50,
+    //time in milliseconds before the marquee will start animating
+    delayBeforeStart: 0,
+    //'left' or 'right'
+    direction: 'left',
+    //true or false - should the marquee be duplicated to show an effect of continues flow
+    duplicated: true
 });
